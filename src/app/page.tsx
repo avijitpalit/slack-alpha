e46ea7f04e274248be4d16592c6ae931f6e1f508
Avijit Palit <avijitpalit3@gmail.com>
2024-03-06 00:36:28 +0530

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,71 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+// import styles from "./page.module.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import './page.css'
+import { Roboto, Nunito, Inter } from 'next/font/google'
+import Link from 'next/link';
+import { useState } from 'react';
+
+const roboto = Roboto({
+    weight: '400',
+    subsets: ['latin'],
+})
+
+const nunito = Nunito({
+    weight: '600',
+    subsets: ['latin'],
+})
+
+const inter = Inter({
+    weight: ['300', '400', '500', '600', '700'],
+    subsets: ['latin'],
+})
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [styledText, setStyledText] = useState('')
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const onInputTextChange = (e: any) => {
+        // console.log(e.target.value);
+        //:alphabet-white-n:
+        //:a:
+        var text = e.target.value
+        var text_2 = ''
+        for (let i = 0; i < text.length; i++) {
+            let alpha = text.charAt(i).toLowerCase()
+            text_2 += alpha == ' ' ? '      ' : `:alphabet-white-${ alpha }:`
+        }
+        setStyledText(text_2)
+        // console.log(text_2);
+    }
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    return (
+        <main className={inter.className}>
+            <Link href="/about" className="about-link">About Me</Link>
+            <div className="text-center my-5">
+                <h1 style={{ fontWeight: 400 }}>Generate Alphabet Emojis <span style={{ fontWeight: '600' }}>for Slack</span></h1>
+                <form className="d-inline-block text-start py-3 px-5 mt-4 form-main" onSubmit={(e: any) => { e.preventDefault(); }}>
+                    <div className="form-row d-flex align-items-center">
+                        <div className="d-flex align-items-center tabs">
+                            <a href="#" className="tab active">Style 1</a>
+                            <a href="#" className="tab">Style 2</a>
+                        </div>
+                    </div>
+                    <div className="form-row position-relative mt-4">
+                        <label className="w-100" htmlFor="input-text">
+                            <input type="text" name="text" id="input-text" className="form-control w-100 input-text" placeholder='Start writing here to see magic...' onChange={onInputTextChange} />
+                        </label>
+                    </div>
+                    <div className="form-row mt-4">
+                        <label htmlFor="" className="w-100">
+                            <textarea className='form-control w-100' name="" rows={ 7 } placeholder='Slack emoji alphabets appears here' value={ styledText } readOnly />
+                        </label>
+                    </div>
+                    <div className="form-row text-end mt-3">
+                        <button className="button">Copy</button>
+                    </div>
+                </form>
+            </div>
+        </main>
+    );
 }
