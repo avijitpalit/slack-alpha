2ed04f89e02b0f5632cf412214ca86e798591d77
Krishna Kumar <krishna.techexactly@gmail.com>
2024-03-08 18:24:28 +0530

    final

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
 import styles from './page.module.css'
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import Link from 'next/Link';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function About() {
   return (
     <main>
-      <h1>About</h1>
-      
+        <div className="container">
+            <div className='text-center mt-5'>
+                <Link className='fs-3' href='/'><FontAwesomeIcon className={styles.backToHome} icon={ faHouse } /></Link>
+            </div>
+            <h1 className='text-center'>About Me</h1>
+            <h6 className="text-center mt-4">Avijit Palit - Full stack web developer</h6>
+            <div className="text-center">
+                <div className={`text-center mt-3 position-relative d-inline-block mx-auto avatar-wrapper ${styles.avatarWrapper}`}>
+                    <img className={styles.avatar} src="https://avijitpalit.netlify.app/static/media/about.bdd7457b9f7a453ec53f.jpg" alt="Me" />
+                    
+                    <a target='_blank' className={`${styles.socialIcons} ${styles.github}`} href="https://github.com/avijitpalit"><FontAwesomeIcon className='fs-2 text-dark' icon={ faGithub } /></a>
+
+                    <a target='_blank' className={`${styles.socialIcons} ${styles.linkedin}`} href="https://www.linkedin.com/in/avijitpalit/"><FontAwesomeIcon className='fs-3 text-dark' icon={ faLinkedin } /></a>
+                </div>
+            </div>
+            <p className='w-50 mx-auto mt-4 text-center'>
+            Taking web development and graphics design as a profession not only fulfills my pocket but also my heart because it has been my passion since my early teenage. <a className='link' target='_blank' href="https://avijitpalit.netlify.app/about">Read more...</a>
+            </p>
+        </div>
     </main>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,12 @@ const inter = Inter({
 export default function Home() {
     const [styledText, setStyledText] = useState('')
     const [normalText, setNormalText] = useState('')
+    const [formValues, setFormValues] = useState({
+        input: '',
+        output: ''
+    })
     const [style, setStyle] = useState(1)
+    const [copyBtnTxt, setCopyBtnTxt] = useState('Copy')
 
     const onInputTextChange = (e: any) => {
         //:alphabet-white-n:
@@ -39,23 +44,29 @@ export default function Home() {
             if(style == 1) text_2 += alpha == ' ' ? '      ' : `:alphabet-white-${ alpha }:`
             else text_2 += alpha == ' ' ? '      ' : `:${ alpha }:`
         }
-        setStyledText(text_2)
+        //setStyledText(text_2)
+        setFormValues({...formValues, input: e.target.value, output: text_2})
     }
 
-    const onStyleChane = (e: any) => {
+    const onStyleChange = (e: any) => {
         setStyle(e.target.checked ? 2 : 1)
         // console.log(style);
         convertText()
     }
 
     const onCopyText = () => {
-        navigator.clipboard.writeText(styledText)
+        navigator.clipboard.writeText(formValues.output)
+        setCopyBtnTxt('Copied');
+        setTimeout(() => {
+            setCopyBtnTxt('Copy')
+        }, 500);
     }
 
     const convertText = () => {
         var text_2 = ''
         // console.log(normalText);
         
+        let normalText = formValues.input
         for (let i = 0; i < normalText.length; i++) {
             let alpha = normalText.charAt(i).toLowerCase()
             if(style == 2) text_2 += alpha == ' ' ? '      ' : `:alphabet-white-${ alpha }:`
@@ -64,8 +75,15 @@ export default function Home() {
         console.log(style);
         // console.log(text_2);
         
-        setStyledText(text_2)
+        // setStyledText(text_2)
+        setFormValues({...formValues, output: text_2})
         // console.log(text_2);
+    }
+
+    const onReset = () => {
+        // console.log(formValues);
+        //document.getElementById("main-form").reset();
+        setFormValues({...formValues, input: '', output: ''})
     }
 
     return (
@@ -73,9 +91,10 @@ export default function Home() {
             <Link href="/about" className="about-link">About Me</Link>
             <div className="text-center my-5">
                 <h1 style={{ fontWeight: 400 }}>Generate Alphabet Emojis <span style={{ fontWeight: '600' }}>for Slack</span></h1>
-                <form className="d-inline-block text-start py-3 px-5 mt-4 form-main" onSubmit={(e: any) => { e.preventDefault(); }}>
+                <form id='main-form' className="d-inline-block text-start py-3 px-5 mt-4 form-main" onSubmit={(e: any) => { e.preventDefault(); }}>
+                    <input type="hidden" name="reset" />
                     <div className="form-row d-flex align-items-center">
-                        <input onChange={ onStyleChane } className='d-none' type="checkbox" name="style" id="style" />
+                        <input onChange={ onStyleChange } className='d-none' type="checkbox" name="style" id="style" />
                         <label htmlFor='style' className="d-flex align-items-center tabs">
                             <span className="tab active">Style 1</span>
                             <span className="tab">Style 2</span>
@@ -83,20 +102,22 @@ export default function Home() {
                     </div>
                     <div className="form-row position-relative mt-4">
                         <label className="w-100" htmlFor="input-text">
-                            <input type="text" name="text" id="input-text" className="form-control w-100 input-text" placeholder='Start writing here to see magic...' onChange={onInputTextChange} />
+                            <input
+                            type="text"
+                            name="text"
+                            id="input-text"
+                            className="form-control w-100 input-text" placeholder='Start writing here to see magic...' onChange={ onInputTextChange }
+                            value={formValues.input} />
                         </label>
                     </div>
                     <div className="form-row mt-4">
                         <label htmlFor="" className="w-100">
-                            <textarea className='form-control w-100' name="" rows={ 7 } placeholder='Slack emoji alphabets appears here' value={ styledText } readOnly />
+                            <textarea className='form-control w-100' name="" rows={ 7 } placeholder='Slack emoji alphabets appears here' value={ formValues.output } readOnly />
                         </label>
                     </div>
                     <div className="form-row text-end mt-3 d-flex gap-1 justify-content-end">
-                        <button onClick={() => {
-                            console.log(style);
-                            
-                        }} className="button">Reset</button>
-                        <button onClick={onCopyText} className="button">Copy</button>
+                        <button onClick={onReset} className="button button-outlined">Reset</button>
+                        <button onClick={onCopyText} className="button">{ copyBtnTxt }</button>
                     </div>
                 </form>
             </div>
